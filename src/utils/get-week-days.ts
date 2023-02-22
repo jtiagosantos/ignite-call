d2ae7f00e21860e857c580b01c70d3525433c7f9
jtiagosantos <tiagosan040@gmail.com>
2023-02-22 14:22:42 -0300

    feat: implement screen and service to time intervals

---
+++ src/utils/get-week-days.ts
@@ -0,0 +1,7 @@
+export const getWeekDays = () => {
+  const formatter = new Intl.DateTimeFormat('pt-BR', { weekday: 'long' })
+
+  return Array.from(Array(7).keys())
+    .map((day) => formatter.format(new Date(Date.UTC(2021, 5, day))))
+    .map((weekDay) => weekDay[0].toUpperCase().concat(weekDay.substring(1)))
+}
