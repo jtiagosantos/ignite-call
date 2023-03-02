866aae6d0530c3070c71adad4ff22262b622b472
jtiagosantos <tiagosan040@gmail.com>
2023-03-02 20:25:05 -0300

    fix: fix returns on endpoint to handle availability times

--- src/pages/api/users/[username]/availability.api.ts
+++ src/pages/api/users/[username]/availability.api.ts
@@ -31,7 +31,7 @@ export default async function handler(
   const isPastDate = referenceDate.endOf('day').isBefore(new Date())
 
   if (isPastDate) {
-    return response.json({ availability: [] })
+    return response.json({ possibleTimes: [], availableTimes: [] })
   }
 
   const userAvailability = await prisma.userTimeInterval.findFirst({
@@ -42,7 +42,7 @@ export default async function handler(
   })
 
   if (!userAvailability) {
-    return response.json({ availability: [] })
+    return response.json({ possibleTimes: [], availableTimes: [] })
   }
 
   const { time_end_in_minutes, time_start_in_minutes } = userAvailability
